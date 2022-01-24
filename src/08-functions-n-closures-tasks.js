7a6fe0c2344b06a9af5843d00aaece45de38d199
Henadzi <68184678+oreshkevich@users.noreply.github.com>
2022-01-24 17:06:48 +0300

    Update 08-functions-n-closures-tasks.js

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -89,8 +89,9 @@ function getPolynom(a, b, c) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const a = func();
+  return () => a;
 }
 
 
@@ -180,8 +181,10 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let s = startFrom - 1;
+  // eslint-disable-next-line no-return-assign
+  return () => s += 1;
 }
 
 
